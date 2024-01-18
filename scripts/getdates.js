8202ab8fedeb8f21284624104f7081099b244714
maria <robertmary_77450@yahoo.com>
2024-01-17 20:13:23 -0700

    #getdates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,4 @@
 const d = new Date();
-console.log(d) 
 const y = d.getFullYear();
-console.log(y)
-getdates.js 
 const copyright_info = document.querySelector("#year");
 copyright_info.innerHTML =` &copy ${y} Maria Oldroyd Texas`;
