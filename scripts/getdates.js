c5f08be661d2be3de3dfc3dfd3c1fcee63db3ba9
maria <robertmary_77450@yahoo.com>
2024-01-18 23:30:12 -0700

    icon

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const d = new Date();
 const y = d.getFullYear();
 const copyright_info = document.querySelector("#year");
-copyright_info.innerHTML =` &copy ${y} Maria Oldroyd Texas`;
+copyright_info.innerHTML = ` &copy ${y} Maria Oldroyd Texas`;
