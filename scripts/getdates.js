0a3a3ab682ae270cd21c0521f69b70e91e067942
maria <robertmary_77450@yahoo.com>
2024-01-19 18:18:30 -0700

    modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,8 @@
 const d = new Date();
 const y = d.getFullYear();
 const copyright_info = document.querySelector("#year");
-copyright_info.innerHTML = ` &copy ${y} Maria Oldroyd Texas`;
+copyright_info.innerHTML = `&copy ${y} Maria Oldroyd Texas`;
+
+const modified =document.querySelector("#lastModified");
+last_modified = document.lastModified;
+modified.innerHTML = `Last Modified: ${last_modified}`;
