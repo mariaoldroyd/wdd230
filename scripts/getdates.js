fc2b6fb4bb948d3b9cc6bbd80f4ff8568e04541e
maria <robertmary_77450@yahoo.com>
2024-01-24 08:18:18 -0700

    button

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,23 @@ copyright_info.innerHTML = `&copy ${y} Maria Oldroyd Texas`;
 const modified =document.querySelector("#lastModified");
 last_modified = document.lastModified;
 modified.innerHTML = `Last Modified: ${last_modified}`;
+
+
+document.addEventListener('DOMContentLoaded',()=> {
+    const hamButton = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+})
+    
+    hamButton.addEventListener('click',() =>{
+      nav.classList.toggle('active');
+      hamButton.classList.toggle('active');
+    })
+
+    // Close the menu when a menu item is clicked
+    document.querySelector('nav a').forEach(item => {
+      item.addEventListener('click', () => {
+        nav.classList.remove('active');
+        menuIcon.classList.remove('active');
+      });
+    });
+    
