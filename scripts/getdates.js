94877295c33da32e91e19343a5fc97c451c592bd
maria <robertmary_77450@yahoo.com>
2024-01-25 11:09:09 -0700

    icon

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -8,21 +8,21 @@ last_modified = document.lastModified;
 modified.innerHTML = `Last Modified: ${last_modified}`;
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const hamButton = document.querySelector('#menu');
-    const nav = document.querySelector('.navigation');
 
-    hamButton.addEventListener('click', () => {
-        nav.classList.toggle('open');
-        hamButton.classList.toggle('open');
-    });
+const hamButton = document.querySelector('#menu');
+const nav = document.querySelector('.navigation');
 
-    // Corrected selector for navigation links
-    document.querySelectorAll('.navigation a').forEach(item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('open');
-            hamButton.classList.remove('open');
-        });
+hamButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// Corrected selector for navigation links
+document.querySelectorAll('.navigation a').forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.remove('open');
+        hamButton.classList.remove('open');
     });
+});
 });
 
