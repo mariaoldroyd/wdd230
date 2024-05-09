d599efd53b43470a24049e0dd62e54fa407bd46b
maria <robertmary_77450@yahoo.com>
2024-05-08 19:03:18 -0600

    js script

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,23 +1,45 @@
 
 
+document.addEventListener("DOMContentLoaded", function() {
     // Get the last modified date of the HTML file
-    alert(document.lastModified);
     const lastModified = new Date(document.lastModified);
 
-    // Populate the last modified date in the designated element
+    
     document.getElementById("lastModified").innerText = formatDate(lastModified);
 
+   // Function to increment and display the visit count
+function incrementVisitCount() {
     
+    let visitCount = localStorage.getItem('visitCount');
 
-    const hamburgerMenu = document.querySelector('#hamburger-menu');
-    const navigation = document.querySelector('.navigation');
+    
+    visitCount = visitCount ? parseInt(visitCount) + 1 : 1;
+   
+    localStorage.setItem('visitCount', visitCount);
+
+    
+    document.getElementById('visitCount').innerText = visitCount;
+}
+window.onload = function() {
+    incrementVisitCount();
+};
+
+    // Hamburger Menu functionality
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const navigation = document.querySelector(".navigation");
 
     // Add event listener to the hamburger icon
-    hamburgerMenu.addEventListener('click', () =>{
-        // Toggle the visibility of the main menu items
-        navigation.classList.toggle('show');
-        hamburgerMenu.classList.toggle('show');
+    hamburgerMenu.addEventListener("click", () => {
+        // Toggle the visibility of the navigation menu
+        navigation.classList.toggle("show-menu");
     });
+});
+
+// Function to format the date in a readable format
+function formatDate(date) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+}
 
 
 
