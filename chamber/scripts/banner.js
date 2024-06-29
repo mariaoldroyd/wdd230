2199bd4d437f05fac2c55d439662d274473c7215
maria <robertmary_77450@yahoo.com>
2024-06-28 22:52:19 -0600

    css

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -9,10 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
         // 1= Monday, 2 = tuesday, 3 = Wednesday
         if (today === 1 || today === 2 || today === 3) {
             const bannerContainer = document.getElementById('banner-container');
-            bannerContainer.appendChild(banner);
+            if (bannerContainer) {
+                bannerContainer.appendChild(banner);
+            }
     
             document.querySelector('.close-banner').addEventListener('click', () => {
                 banner.style.display = 'none';
             });
         }
     });
+
+     // Function to make the events scroll
+     function scrollEvents() {
+        const eventsWrapper = document.getElementById('eventsWrapper');
+        const firstEvent = eventsWrapper.firstElementChild;
+        const eventHeight = firstEvent.offsetHeight;
+        eventsWrapper.style.transition = 'transform 4s linear';
+        eventsWrapper.style.transform = `translateY(-${eventHeight}px)`;
+
+        // After the transition, move the first event to the bottom and reset transform
+        setTimeout(() => {
+            eventsWrapper.style.transition = 'none';
+            eventsWrapper.style.transform = 'none';
+            eventsWrapper.appendChild(firstEvent);
+        }, 1000);
+    }
+
+    // Scroll events every 2 seconds
+    setInterval(scrollEvents, 2000);
