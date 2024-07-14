bfa2552df158ffb8fffce0ebb551c11c173840be
maria <robertmary_77450@yahoo.com>
2024-07-13 23:17:14 -0600

    responsive

---
+++ scoots/scripts/darkmode.js
@@ -0,0 +1,5 @@
+// JavaScript for Dark Mode Toggle
+const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
