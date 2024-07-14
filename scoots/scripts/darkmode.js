d060383d3ce18ea86ac60f67a1ab10b5c162e5db
maria <robertmary_77450@yahoo.com>
2024-07-14 01:03:20 -0600

    map

--- scoots/scripts/darkmode.js
+++ scoots/scripts/darkmode.js
@@ -1,5 +1,11 @@
-// JavaScript for Dark Mode Toggle
-const toggleButton = document.getElementById('dark-mode-toggle');
-toggleButton.addEventListener('click', () => {
+document.getElementById('dark-mode-toggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+    document.querySelector('.nav-links a').classList.toggle('dark-mode');
+    document.querySelector('.hero img').classList.toggle('dark-mode');
 });
+
+function closeMessage() {
+    document.getElementById('high-temp-message').style.display = 'none';
+}
